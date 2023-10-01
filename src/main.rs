55db4f8cb4296b59bcdc9913dc853b145f133996
Ticua <ticua@ticua.ar>
2023-10-01 19:54:52 -0300

    4st stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_conn(stream: &mut TcpStream) {
+    stream
+        .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+        .expect("Failed to send msg");
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,7 +17,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                handle_conn(&mut stream);
                 println!("accepted new connection");
             }
             Err(e) => {
