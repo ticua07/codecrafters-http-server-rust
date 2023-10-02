620482e03d3e9e61aa66ec395e3ba92d1ad4f342
Ticua <ticua@ticua.ar>
2023-10-02 00:49:50 -0300

    6st stage ???

--- src/utils.rs
+++ src/utils.rs
@@ -21,11 +21,14 @@ impl HTTPRequest {
 }
 
 pub fn create_response(code: String, content_type: String, body: String) -> String {
-    let cont_len = body.chars().count();
+    let mut content = body.clone();
+    content.push_str("\r");
+    let cont_len = content.chars().count();
+
     println!("PLEASE");
     return format!(
         "HTTP/1.1 {}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
-        code, content_type, cont_len, body
+        code, content_type, cont_len, content
     );
 }
 
