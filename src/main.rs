209df7d4f8c873de1997d019a5543f4ec543f8b5
Ticua <ticua@ticua.ar>
2023-10-01 23:58:45 -0300

    5st stage 1

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,57 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
+use utils::{HTTPMethod, NOT_FOUND_RESPONSE, OK_RESPONSE};
+
+use crate::utils::HTTPRequest;
+mod utils;
+
 fn handle_conn(stream: &mut TcpStream) {
+    let mut request_buffer = [0; 512];
+
+    stream.read(&mut request_buffer).unwrap();
+
+    let request_str = std::str::from_utf8(&request_buffer).unwrap();
+    let req = parse_request(request_str);
+
+    let response = match req.path.as_str() {
+        "/" => String::from(OK_RESPONSE),
+        _ => String::from(NOT_FOUND_RESPONSE),
+    };
+
     stream
-        .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-        .expect("Failed to send msg");
+        .write(response.as_bytes())
+        .expect("Couldn't return response");
+}
+
+fn parse_request(request_string: &str) -> HTTPRequest {
+    let lines: Vec<&str> = request_string.lines().collect();
+    let mut first_line = lines[0].split_ascii_whitespace();
+
+    let method = match first_line.next().unwrap() {
+        "GET" => HTTPMethod::GET,
+        "POST" => HTTPMethod::POST,
+        _ => HTTPMethod::INVALID,
+    };
+
+    let route = first_line.next().unwrap();
+    println!("{} -> {}", method, route);
+
+    HTTPRequest::new(method, route.to_string())
 }
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 handle_conn(&mut stream);
+                stream
+                    .write(b"HTTP/1.1 200 OK\r\n\r\n")
+                    .expect("Failed to send msg");
                 println!("accepted new connection");
             }
             Err(e) => {
---
+++ src/utils.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+pub struct HTTPRequest {
+    pub method: HTTPMethod,
+    pub path: String,
+}
+
+#[derive(Debug)]
+pub enum HTTPMethod {
+    GET,
+    POST,
+    INVALID,
+}
+
+pub const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
+pub const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
+impl HTTPRequest {
+    pub fn new(method: HTTPMethod, path: String) -> Self {
+        Self { method, path }
+    }
+}
+
+impl std::fmt::Display for HTTPMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HTTPMethod::GET => write!(f, "GET"),
+            HTTPMethod::POST => write!(f, "POST"),
+            HTTPMethod::INVALID => write!(f, "INVALID"),
+        }
+    }
+}
